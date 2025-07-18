7535fef6edd1a59220e30bdc9112a37faa81e0c9
Naman Kemani <namankemani@gmail.com>
2025-07-18 10:12:53 +0530

    My complete FullStack app

---
+++ frontend/src/routers/AdminRoute.jsx
@@ -0,0 +1,14 @@
+
+import { Navigate, Outlet } from 'react-router-dom';
+
+// eslint-disable-next-line react/prop-types
+const AdminRoute = ({children}) => {
+  const token = localStorage.getItem('token');
+  if(!token) {
+    return <Navigate to="/admin"/>
+  }
+  return children ?  children : <Outlet/>;
+}
+
+
+export default AdminRoute
